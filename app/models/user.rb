d45755eeae079c5560c10100c794427fc5b20dbf
ama-01311 <76988330+ama-01311@users.noreply.github.com>
2021-02-02 17:57:47 +0900

    userモデル　バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :lname_full_width, presence: true
+  validates :fname_full_width, presence: true
+  validates :lname_kana, presence: true
+  validates :fname_kana, presence: true
+  validates :birth_date, presence: true
 end
