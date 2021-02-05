c538b5a3607fb37ef92428ece862dd6bace27df2
ama-01311 <76988330+ama-01311@users.noreply.github.com>
2021-02-05 12:30:57 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :item_name
+    validates :item_description
+    validates :item_category_id
+    validates :item_state_id
+    validates :delivery_side_id
+    validates :delivery_prefecture_id
+    validates :delivery_days_id
+    validates :image
+
+    validates :selling_price format: { with: /\A(?=.*?[\d])[a-z\d]+\z/i, message: "半角数字で入力してください"}
+  end
+  validates :user_id foreign_key: true
+
 end
