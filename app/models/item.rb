9dee213c1e9151218e04c06f1b3caef146515576
ama-01311 <76988330+ama-01311@users.noreply.github.com>
2021-02-05 19:15:50 +0900

    form表示、ActiveHash実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,23 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions 
+    belongs_to :item_category
+    belongs_to :item_state
+    belongs_to :delivery_side
+    belongs_to :delivery_prefecture
+    belongs_to :delivery_days
+  
+
   belongs_to :user
   has_one_attached :image
+
+  with_options numericality: {other_than: 1} do
+    validates :item_category
+    validates :item_state
+    validates :delivery_side
+    validates :delivery_prefecture
+    validates :delivery_days
+  end
+  
 
   with_options presence: true do
     validates :item_name
