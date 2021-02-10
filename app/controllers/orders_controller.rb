73fc4f28fc7d3b26d834a63ad60ba4562d9ac75d
ama-01311 <76988330+ama-01311@users.noreply.github.com>
2021-02-10 18:52:36 +0900

    ビューの表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,8 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
   def index
-  
+    @item = Item.find(params[:item_id])
+    @orders = Order.new
   end
+
 end
