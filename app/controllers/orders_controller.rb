7070d62037acf2a94d0ea90b6531e6a9c654fc5c
ama-01311 <76988330+ama-01311@users.noreply.github.com>
2021-02-11 15:40:57 +0900

    Formオブジェクトの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,23 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @orders = Order.new
+    @order_address = OrderAddress.new
   end
 
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    #binding.pry
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+    render :index
+    end
+  end
+
+  def order_params
+    params.require(:order_address).permit(:delivery_post_number, :delivery_prefecture_id, :delivery_municipality, :delivery_building, :delivery_address, :delivery_prefecture_id, :phone_number)
+    .merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,4 @@
 class Address < ApplicationRecord
   belongs_to :order
 
-  with_options presence: true do
-    validates :delivery_post_number
-    validates :delivery_prefecture_id, numericality: {other_than: 1 }
-    validates :delivery_municipality
-    validates :delivery_address
-    validates :phone_number
-  end
-
 end
